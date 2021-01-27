68ae18a99212a351f087cc3ae27513d6c5bb0104
metalzoa <metalzoa@naver.com>
2021-01-27 16:52:50 +0900

    여러가지 app 추가

--- src/main/java/test2/Application.java
+++ src/main/java/test2/Application.java
@@ -1,19 +1,13 @@
-package test;
+package test2;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import javax.swing.*;
 
 @SpringBootApplication
 // 아래 3개가 위에 SpringBootApplication 하나의 설정에 모두 포함된 상태
-//@SpringBootConfiguration
-//@ComponentScan
-//@EnableAutoConfiguration
+//@SpringBootConfiguration // 일반 @Configuration 과 거의 동일
+//@ComponentScan  // 1단계 빈 등록
+//@EnableAutoConfiguration // 2단계 빈등록, 서블릿 설정을 이곳에서 하게됨
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run( Application.class, args);
